cd00c64ae4b22aeeb37e0f95a19c98cef842b2fa
i1rr <rivan1986@gmail.com>
2022-06-15 22:25:28 +1000

    getAcc test + test tweaks

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,12 +3,13 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"github.com/techschool/simplebank/util"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
@@ -25,4 +26,23 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	acc1 := createRandomAccount(t)
+	acc2, err := testQueries.GetAccount(context.Background(), acc1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, acc2)
+
+	require.Equal(t, acc1.ID, acc2.ID)
+	require.Equal(t, acc1.Owner, acc2.Owner)
+	require.Equal(t, acc1.Balance, acc2.Balance)
+	require.Equal(t, acc1.Currency, acc2.Currency)
+	require.WithinDuration(t, acc1.CreatedAt, acc2.CreatedAt, time.Second)
 }
